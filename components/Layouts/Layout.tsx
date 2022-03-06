356dab6e55ecc94086f4c98035c41c91f8e84eae
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-06 09:59:02 +0800

    first commit

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -2,7 +2,8 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Image from "next/image";
 import Header from "components/Hearder";
-import pokeballbg from "public/pokemon-bg.jpg";
+import pokeballbg from "public/pokeballbg.png";
+import pokeball from "public/pokeball-edited.png";
 
 type Props = {
   children: any;
@@ -11,7 +12,16 @@ type Props = {
 const Layouts = ({ children }: Props) => {
   return (
     <Box width="100%" position="relative" zIndex="0">
-      <Box width="100%" height="100%" position="absolute" zIndex="-1">
+      <Box
+        width="100%"
+        height="100%"
+        position="absolute"
+        zIndex="-1"
+        top="0px"
+        bottom="0px"
+        left="0px"
+        right="0px"
+      >
         <Image
           src={pokeballbg}
           alt="background"
