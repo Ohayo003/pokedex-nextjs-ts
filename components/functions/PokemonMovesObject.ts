4206b0dca3dc85451fac7dd0c511017593126745
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-11 09:22:58 +0800

    removed not used functions

--- components/functions/PokemonMovesObject.ts
+++
@@ -1,9 +0,0 @@
-import { IPokemonMoves } from "components/interface/pokemonData";
-
-export default function PokemonMovesObject(data: any): IPokemonMoves {
-  return {
-    name: data.name,
-    power: data.power,
-    accuracy: data.accuracy,
-  };
-}
